418d9cbeefa9e01ad9bf7f46b9c3fe8dd07335d4
Bogdan Anghel <bogdan@joyphoto.ro>
2015-10-03 00:02:45 +0100

    first update

---
+++ Tester/Sandbox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using FastColoredTextBoxNS;
+
+namespace Tester
+{
+    public partial class Sandbox : Form
+    {
+        private FastColoredTextBox fctb;
+
+        public Sandbox()
+        {
+            InitializeComponent();
+
+            fctb = new FastColoredTextBox() { Dock = DockStyle.Fill, Parent = this, Language = Language.XML, HighlightingRangeType = HighlightingRangeType.AllTextRange, ShowFoldingLines = true};
+        }
+    }
+}
---
+++ wSQL/frmMain.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace wSQL
+{
+   partial class frmMain
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+         this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+         this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+         this.panel3 = new System.Windows.Forms.Panel();
+         this.splitter2 = new System.Windows.Forms.Splitter();
+         this.panel1 = new System.Windows.Forms.Panel();
+         this.mainMenu = new System.Windows.Forms.MenuStrip();
+         this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+         this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+         this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+         this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.panel3.SuspendLayout();
+         this.mainMenu.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // statusStrip1
+         // 
+         this.statusStrip1.Location = new System.Drawing.Point(0, 470);
+         this.statusStrip1.Name = "statusStrip1";
+         this.statusStrip1.Size = new System.Drawing.Size(942, 22);
+         this.statusStrip1.TabIndex = 2;
+         this.statusStrip1.Text = "statusStrip1";
+         // 
+         // toolStrip1
+         // 
+         this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+         this.toolStrip1.Name = "toolStrip1";
+         this.toolStrip1.Size = new System.Drawing.Size(942, 25);
+         this.toolStrip1.TabIndex = 4;
+         this.toolStrip1.Text = "toolStrip1";
+         // 
+         // panel3
+         // 
+         this.panel3.Controls.Add(this.fastColoredTextBox1);
+         this.panel3.Controls.Add(this.splitter2);
+         this.panel3.Controls.Add(this.panel1);
+         this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.panel3.Location = new System.Drawing.Point(0, 49);
+         this.panel3.Name = "panel3";
+         this.panel3.Size = new System.Drawing.Size(942, 421);
+         this.panel3.TabIndex = 6;
+         // 
+         // splitter2
+         // 
+         this.splitter2.Dock = System.Windows.Forms.DockStyle.Bottom;
+         this.splitter2.Location = new System.Drawing.Point(0, 318);
+         this.splitter2.Name = "splitter2";
+         this.splitter2.Size = new System.Drawing.Size(942, 3);
+         this.splitter2.TabIndex = 1;
+         this.splitter2.TabStop = false;
+         // 
+         // panel1
+         // 
+         this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+         this.panel1.Location = new System.Drawing.Point(0, 321);
+         this.panel1.Name = "panel1";
+         this.panel1.Size = new System.Drawing.Size(942, 100);
+         this.panel1.TabIndex = 0;
+         // 
+         // mainMenu
+         // 
+         this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+         this.mainMenu.Location = new System.Drawing.Point(0, 0);
+         this.mainMenu.Name = "mainMenu";
+         this.mainMenu.Size = new System.Drawing.Size(942, 24);
+         this.mainMenu.TabIndex = 10;
+         this.mainMenu.Text = "menuStrip1";
+         // 
+         // fileToolStripMenuItem
+         // 
+         this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+         this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+         this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+         this.fileToolStripMenuItem.Text = "File";
+         // 
+         // exitToolStripMenuItem
+         // 
+         this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+         this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+         this.exitToolStripMenuItem.Text = "Exit";
+         this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+         // 
+         // fastColoredTextBox1
+         // 
+         this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+         this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(179, 14);
+         this.fastColoredTextBox1.BackBrush = null;
+         this.fastColoredTextBox1.CharHeight = 14;
+         this.fastColoredTextBox1.CharWidth = 8;
+         this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+         this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+         this.fastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.fastColoredTextBox1.IsReplaceMode = false;
+         this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 0);
+         this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+         this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+         this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+         this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+         this.fastColoredTextBox1.Size = new System.Drawing.Size(942, 318);
+         this.fastColoredTextBox1.TabIndex = 4;
+         this.fastColoredTextBox1.Text = "fastColoredTextBox1";
+         this.fastColoredTextBox1.Zoom = 100;
+         // 
+         // toolStripMenuItem1
+         // 
+         this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+         this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+         // 
+         // saveToolStripMenuItem
+         // 
+         this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+         this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.saveToolStripMenuItem.Text = "Save";
+         // 
+         // saveAsToolStripMenuItem
+         // 
+         this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+         this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.saveAsToolStripMenuItem.Text = "Save as ...";
+         // 
+         // newToolStripMenuItem
+         // 
+         this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+         this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.newToolStripMenuItem.Text = "New";
+         // 
+         // toolStripMenuItem2
+         // 
+         this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+         this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+         // 
+         // openToolStripMenuItem
+         // 
+         this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+         this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.openToolStripMenuItem.Text = "Open";
+         // 
+         // frmMain
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(942, 492);
+         this.Controls.Add(this.panel3);
+         this.Controls.Add(this.toolStrip1);
+         this.Controls.Add(this.statusStrip1);
+         this.Controls.Add(this.mainMenu);
+         this.MainMenuStrip = this.mainMenu;
+         this.Name = "frmMain";
+         this.Text = "Form1";
+         this.panel3.ResumeLayout(false);
+         this.mainMenu.ResumeLayout(false);
+         this.mainMenu.PerformLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.StatusStrip statusStrip1;
+      private System.Windows.Forms.ToolStrip toolStrip1;
+      private System.Windows.Forms.Panel panel3;
+      private System.Windows.Forms.MenuStrip mainMenu;
+      private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+      private System.Windows.Forms.Panel panel1;
+      private System.Windows.Forms.Splitter splitter2;
+      private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+      private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+      private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+      private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+      private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+      private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+   }
+}
+
