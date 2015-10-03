e678fae83e3c2bf203840cc9151947f20c8ed113
Marcel Popescu <mdpopescu@gmail.com>
2015-10-03 19:29:35 +0100

    Added a symbol table implementation based on a dictionary

---
+++ wSQL.Language/Services/SymbolsTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using wSQL.Language.Contracts;
+
+namespace wSQL.Language.Services
+{
+  public class SymbolsTable : Symbols
+  {
+    public void Declare(string name)
+    {
+      if (dict.ContainsKey(name))
+        throw new Exception("Variable has already been declared: " + name);
+
+      dict[name] = null;
+    }
+
+    public void Set(string name, object value)
+    {
+      dict[name] = value;
+    }
+
+    public object Get(string name)
+    {
+      return dict[name];
+    }
+
+    //
+
+    private readonly Dictionary<string, object> dict = new Dictionary<string, object>();
+  }
+}
